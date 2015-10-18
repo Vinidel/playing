c4f5becfe638441854dcc058788281b3c841fd4a
Vinícius Deláscio <vinidroid@gmail.com>
2015-10-18 15:11:38 +1100

    init

---
+++ app/models/Type.java
@@ -0,0 +1,19 @@
+package models;
+
+import java.util.*;
+import play.data.validation.Constraints.*;
+import play.db.ebean.*;
+
+import javax.persistence.*;
+
+  public class Type extends Model{
+
+      public Long id;
+      public String Name;
+
+
+
+      
+
+
+  }
---
+++ notes.js
@@ -0,0 +1,2 @@
+ - Play templates are compiled to standard Java methods, here as views.html.index.render(String message).
+ - The Form object encapsulates a HTML, what does that mean?
